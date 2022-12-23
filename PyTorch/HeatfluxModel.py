c54c60be4886982ca5e23ed5e69cc0fd6e3e05d1
Milan Holec <homijan@gmail.com>
2022-12-23 17:00:12 +0100

    PyTorch heatflux notebook does training
    Heat flux evaluation seems to fail for now.

--- PyTorch/HeatfluxModel.py
+++ PyTorch/HeatfluxModel.py
@@ -7,12 +7,17 @@ class AlphaModel(pl.LightningModule):
 
 ### Model ###
     def __init__(self, Nfeatures, N1, N2, Ntargets, scaling):
+        super(AlphaModel, self).__init__() # TODO: if not "cannot assign module before Module.__init__() call"
         # Initialize layers
         self.fc1 = torch.nn.Linear(Nfeatures, N1)
         self.fc2 = torch.nn.Linear(N1, N2)
         self.fc3 = torch.nn.Linear(N2, Ntargets)
         # Keep data scaling
-        self.scaling = scaling    
+        self.scaling = scaling
+        # TODO: improve evaluation of Npoints
+        self.Npoints = int(Nfeatures / 5)
+        # TODO: better place to define mse_loss
+        self.mse_loss = torch.nn.MSELoss(reduction = 'mean')
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
@@ -22,8 +27,8 @@ class AlphaModel(pl.LightningModule):
 
 ### The Optimizer ### 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters, lr=l_rate)
-        #optimizer = torch.optim.SGD(self.parameters(), lr=l_rate)
+        #optimizer = torch.optim.Adam(self.parameters, lr=0.05)#l_rate) # TODO: should be a parameter
+        optimizer = torch.optim.SGD(self.parameters(), lr=0.05)#l_rate) # TODO: should be a parameter
         return optimizer
 
 ### Training ### 
@@ -31,10 +36,10 @@ class AlphaModel(pl.LightningModule):
         x, y = batch
         # Evaluate physical model and apply data-scaling
         mean = self.scaling['Qimpact']['mean']
-        sted = self.scaling['Qimpact']['std']
+        std = self.scaling['Qimpact']['std']
         logits = (self.heatflux_model(x) - mean) / std
         # Evaluate loss comparing to the kinetic heat flux in y
-        loss = mse_loss(logits, y)
+        loss = self.mse_loss(logits, y)
         # Add logging
         logs = {'loss': loss}
         return {'loss': loss, 'log': logs}
@@ -44,10 +49,10 @@ class AlphaModel(pl.LightningModule):
         x, y = batch
         # Evaluate physical model and apply data-scaling
         mean = self.scaling['Qimpact']['mean']
-        sted = self.scaling['Qimpact']['std']
+        std = self.scaling['Qimpact']['std']
         logits = (self.heatflux_model(x) - mean) / std
         # Evaluate loss comparing to the kinetic heat flux in y
-        loss = mse_loss(logits, y)
+        loss = self.mse_loss(logits, y)
         return {'val_loss': loss}
 
     # Define validation epoch end
@@ -62,10 +67,10 @@ class AlphaModel(pl.LightningModule):
         # Evaluate physical model and apply data-scaling
         heatflux_model = self.heatflux_model(x)
         mean = self.scaling['Qimpact']['mean']
-        sted = self.scaling['Qimpact']['std']
+        std = self.scaling['Qimpact']['std']
         logits = (heatflux_model - mean) / std
         # Evaluate loss comparing to the kinetic heat flux in y
-        loss = mse_loss(logits, y)
+        loss = self.mse_loss(logits, y)
         # Compare model to the kinetic heat flux in y
         correct = torch.sum(logits == y.data)
         # TODO: change this global variable
@@ -89,12 +94,12 @@ class AlphacModel(AlphaModel):
         # Extract features for modified local heat flux
         featureName = ['Z', 'T', 'gradT', 'Kn', 'n']
         feature = {}
-        ip = int(Npoints / 2)
+        ip = int(self.Npoints / 2)
         i = 0
         for name in featureName:
             mean, std = self.scaling[name]['mean'], self.scaling[name]['std']
             # Batch values of the feature at xc
-            feature[name] = x[:, i * Npoints + ip] * std + mean
+            feature[name] = x[:, i * self.Npoints + ip] * std + mean
             #feature[name] = x[:, i * Npoints:(i+1) * Npoints] * std + mean
             i = i+1
         # Get alphas
@@ -103,7 +108,7 @@ class AlphacModel(AlphaModel):
         #Kn = feature['Kn']
         Z = feature['Z']; T = feature['T']; gradT = feature['gradT']
         kQSH = 6.1e+02 # scaling constant consistent with SCHICK
-        heatflux_model = - alphac * kQSH / Z * ((Z + 0.24)/(Z + 4.2))\
+        heatflux_model = - alphac * kQSH / Z * ((Z + 0.24) / (Z + 4.2))\
           * T**2.5 * gradT
         return heatflux_model
 
@@ -117,12 +122,12 @@ class AlphacAlphanModel(AlphaModel):
         # Extract features for modified local heat flux
         featureName = ['Z', 'T', 'gradT', 'Kn', 'n']
         feature = {}
-        ip = int(Npoints / 2)
+        ip = int(self.Npoints / 2)
         i = 0
         for name in featureName:
             mean, std = self.scaling[name]['mean'], self.scaling[name]['std']
             # Batch values of the feature at xc
-            feature[name] = x[:, i * Npoints + ip] * std + mean
+            feature[name] = x[:, i * self.Npoints + ip] * std + mean
             #feature[name] = x[:, i * Npoints:(i+1) * Npoints] * std + mean
             i = i+1
         # Get alphas
@@ -132,6 +137,6 @@ class AlphacAlphanModel(AlphaModel):
         #Kn = feature['Kn']
         Z = feature['Z']; T = feature['T']; gradT = feature['gradT']
         kQSH = 6.1e+02 # scaling constant consistent with SCHICK
-        heatflux_model = - alphac * kQSH / Z * ((Z + 0.24)/(Z + 4.2))\
+        heatflux_model = - alphac * kQSH / Z * ((Z + 0.24) / (Z + 4.2))\
           * T**(2.5 / (1.0 + np.exp(alphan))) * gradT
         return heatflux_model
